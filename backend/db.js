9bd611dc7bb1663ea785a4869ba25e56396d46fd
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2021-12-20 18:02:00 +0530

    Lecture-43 iNotebook db & Express Server Setup

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const mongoURI="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+const connectToMongo=()=>{
+  //it is a inbuilt function to connect to mongoose with some arguments
+  mongoose.connect(mongoURI, 
+    // callback function to deal with async functionality and it is passed as argument
+    ()=>{
+      console.log("Connected to mongo successfully");
+  })
+}
+
+module.exports=connectToMongo;
