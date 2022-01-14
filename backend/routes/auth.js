99452bdce88dc7776b9afa06ccb0614616aa5859
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2022-01-14 22:31:35 +0530

    Lecture-50 Creating Login Endpoint & sending Authtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,26 +25,69 @@ router.post('/createuser', [
     }
 
     // hashing of password
-    const salt=await bcrypt.genSalt(10);
-    const securePassword=await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const securePassword = await bcrypt.hash(req.body.password, salt);
 
     // create A new User
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password:securePassword
+      password: securePassword
     })
 
     // returning user id in Token
-    const JWT_secret="Rishiisa@boy";
-    const data={id:user.id};
+    const JWT_secret = "Rishiisa@boy";
+    const data = { id: user.id };
     const auth_token = jwt.sign(data, JWT_secret);
-    res.json({auth_token});
+    res.json({ auth_token });
   }
   catch (error) {
     console.error(error.message);
-    res.status(500).send("Some erroe occured");
+    res.status(500).send("Internal server error");
   }
+})
+
+
+// Login a User using credential.  "/api/auth/login".   NO Login Required.
+router.post('/login', [
+  body('email', 'Enter valid email').isEmail(),
+  body('password', 'password can not be blank').exists(),
+], async (req, res) => {
+  // Check for vaidation according to the rule defined at line no. 53, 54;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // destructure the email and password from body request
+  const { email, password } = req.body;
+
+  try {
+    // Checking whether email is exist or not 
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Please try to login using correct credentials" });
+    }
+
+    // Now Comparing password with help of bcryptjs
+    const comparepassword = await bcrypt.compare(password, user.password);
+    if (!comparepassword) {
+      return res.status(400).json({ error: "Please try to login using correct credentials" });
+    }
+
+    // Now if user enter coorect password and login then user got logged in;
+    // And We will send authtoken to user;
+    // returning user id in Token
+    const JWT_secret = "Rishiisa@boy";
+    const data = { id: user.id };
+    const auth_token = jwt.sign(data, JWT_secret);
+    res.json({ auth_token });
+  }
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+
 })
 
 module.exports = router
