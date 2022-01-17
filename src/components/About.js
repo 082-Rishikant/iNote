783c7b6ab6e73f4c38666be6c87e2e020bd7763f
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2022-01-17 11:06:21 +0530

    Lecture-59 useLocation Hook in React-Router-dom

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,9 @@
 import React, {useContext, useEffect} from 'react';
-import NoteContext from '../context/notes/NoteContext';
 
 function About() {
-  const a = useContext(NoteContext)
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
   return (
     <div>
-      <h1>This is About {a.state.name} and his Roll no. {a.state.Roll_No}</h1>
+      <h1>This is About Page</h1>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,22 +1,8 @@
 import NoteContext from "./NoteContext";
-import { useState } from "react";
 
 const NoteState=(props)=>{
-  const s1={
-    "name":"Rishikant Patel",
-    "Roll_No":205120082
-  }
-  const [state, setState] = useState(s1);
-  const update=()=>{
-    setTimeout(() => {
-      setState({
-        name:"Devansh Patel",
-        Roll_No:"Pata nahi"
-      })
-    }, [5000]);
-  } 
   return(
-    <NoteContext.Provider value={{state, update}}>
+    <NoteContext.Provider value={{}}>
       {props.children}
     </NoteContext.Provider>
   )
