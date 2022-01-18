fe0e183ac8cd6c45457b03814317eba0098b325a
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2022-01-18 11:15:16 +0530

    Lecture-60 iNoteboo fetching Note from context(Context API)

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,8 +1,30 @@
 import NoteContext from "./NoteContext";
+import {useState} from 'react';
 
 const NoteState=(props)=>{
+  const notesInitially=[
+    {
+      "_id": "61e3e72e99859bf60a42918b",
+      "user": "61dfd82086e39b0746891dca",
+      "title": "Morning routine",
+      "description": "wake up early",
+      "tag": "time table",
+      "date": "2022-01-16T09:36:46.173Z",
+      "__v": 0
+    },
+    {
+      "_id": "61e3e72e99859bf60a42918b",
+      "user": "61dfd82086e39b0746891dca",
+      "title": "Morning routine",
+      "description": "wake up early",
+      "tag": "time table",
+      "date": "2022-01-16T09:36:46.173Z",
+      "__v": 0
+    }
+  ];
+  const [notes, setNotes] = useState(notesInitially)
   return(
-    <NoteContext.Provider value={{}}>
+    <NoteContext.Provider value={{notes, setNotes}}>
       {props.children}
     </NoteContext.Provider>
   )
